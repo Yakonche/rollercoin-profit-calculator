6c23597ae08d72053e4f5f62c559996dfbea484d
Yakonche <60564904+Yakonche@users.noreply.github.com>
2021-03-09 11:10:25 +0100

    addition of the estimate of the value in crypto

--- rollercoin_profit_calculator_french.py
+++ rollercoin_profit_calculator_french.py
@@ -23,20 +23,23 @@ mois = (365.25/12)*jour
 an = mois*12
 
 earnings = []
+earnings_crypto = []
 for i, (network_power, reward, price) in enumerate(zip(network_powers, rewards, prices)):
     currency_gain = reward * (current_hashrate / network_power)
     earnings.append(currency_gain * float(price))
+    earnings_crypto.append(currency_gain)
 
 max_index = earnings.index(max(earnings))
 
-print("\n---------------------------\n\n{} est la crypto-monnaie la plus rentable à exploiter.\n{} € de revenu par bloc.\n".format(names[max_index], earnings[max_index]))
+print("\n---------------------------\n\n{} est la crypto-monnaie la plus rentable à exploiter.\nAvec {} € de revenu / bloc.\nOu {} {} / bloc.\n".format(names[max_index], earnings[max_index], earnings_crypto[max_index], names[max_index]))
 
 earnings_second = earnings[max_index] / (5*minute)
+earnings_cryptot = earnings_crypto[max_index] / (5*minute)
 print("Soit environ :")
-print(str(earnings_second*heure) + " € / heure")
-print(str(earnings_second*jour) + " € / jour")
-print(str(earnings_second*semaine) + " € / semaine")
-print(str(earnings_second*mois) + " € / mois")
-print(str(earnings_second*an) + " € / an")
+print(str(earnings_second*heure) + " € / heure" + " ,soit " + str(earnings_cryptot*heure) + " " + names[max_index])
+print(str(earnings_second*jour) + " € / jour" + " ,soit " + str(earnings_cryptot*jour) + " " + names[max_index])
+print(str(earnings_second*semaine) + " € / semaine" + " ,soit " + str(earnings_cryptot*semaine) + " " + names[max_index])
+print(str(earnings_second*mois) + " € / mois" + " ,soit " + str(earnings_cryptot*mois) + " " + names[max_index])
+print(str(earnings_second*an) + " € / an" + " ,soit " + str(earnings_cryptot*an) + " " + names[max_index])
 
 input("Appuyez sur la touche Entrée pour fermer la fenêtre.")
