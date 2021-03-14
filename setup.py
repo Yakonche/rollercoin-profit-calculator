aef790e32401049bdb6918d1fc3820245f8f8225
the_z <the_z@znel.org>
2021-03-14 01:40:56 +0000

    Add babel build process

--- setup.py
+++ setup.py
@@ -1,5 +1,37 @@
-from cx_Freeze import setup, Executable
 import os.path
+
+from cx_Freeze import setup, Executable, build
+from babel.messages import frontend as babel
+
+
+class BuildWithCompile(build):
+    def run(self):
+        compiler = babel.compile_catalog(self.distribution)
+        compiler.domain = ["rc_profit_calc"]
+        compiler.directory = "./locale"
+        compiler.run()
+        super().run()
+
+
+def list_mo_files(folder, domain):
+    mo_files = []
+    for locale in os.listdir(folder):
+        if os.path.isdir(os.path.join(folder, locale)):
+            mo_files.append(os.path.join(folder, locale, "LC_MESSAGES", domain + ".mo"))
+    return mo_files
+
+
+SRC_PATH = os.path.dirname(__file__)
+
+include_files = [
+    (os.path.join(SRC_PATH, './locale'), 'locale/'),
+]
+
+for mo_file in list_mo_files("locale", "rc_profit_calc"):
+    include_files.append(
+        (os.path.join(SRC_PATH, mo_file), mo_file),
+    )
+
 
 executables = [
     Executable(
@@ -14,9 +46,9 @@ options = {
     'build_exe': {
         'packages': ["os"],
         'optimize': 2,
-        'include_files': []
-        }
+        'include_files': include_files
     }
+}
 
 setup(
     options=options,
@@ -24,5 +56,12 @@ setup(
     version="0.0.5",
     description="Calculator that allows you to determine which cryptocurrency "
                 "is the most profitable to mine on the rollercoin.com site",
-    executables=executables
-    )
+    executables=executables,
+    cmdclass={
+        'build': BuildWithCompile,
+        'compile_catalog': babel.compile_catalog,
+        'extract_messages': babel.extract_messages,
+        'init_catalog': babel.init_catalog,
+        'update_catalog': babel.update_catalog
+    }
+)
